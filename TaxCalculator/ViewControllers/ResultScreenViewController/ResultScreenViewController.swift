9b81008db00838ea87fac096c9fd5abbcef2d61e
smarter-solutions6 <smarter.solutions6@gmail.com>
2017-08-09 12:02:07 +0500

    pdf converted code of a uiview is added

--- TaxCalculator/ViewControllers/ResultScreenViewController/ResultScreenViewController.swift
+++ TaxCalculator/ViewControllers/ResultScreenViewController/ResultScreenViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import QuartzCore
+import MessageUI
+import MobileCoreServices
 
-class ResultScreenViewController: UIViewController {
+class ResultScreenViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
     @IBOutlet weak var netTaxableIncome: UITextField!
     @IBOutlet weak var taxUpto: UITextField!
@@ -117,6 +120,124 @@ class ResultScreenViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
 
+    @IBAction func savePdf(_ sender: UIButton) {
+        createPdfFromView(aView: self.view, saveToDocumentsWithFileName: "myFile.pdf")
+    }
+    
+    func createPdfFromView(aView: UIView, saveToDocumentsWithFileName aFilename: String)
+    {
+        // Creates a mutable data object for updating with binary data, like a byte array
+//        NSMutableData *pdfData = [NSMutableData data];
+//        
+//        // Points the pdf converter to the mutable data object and to the UIView to be converted
+//        UIGraphicsBeginPDFContextToData(pdfData, aView.bounds, nil);
+//        UIGraphicsBeginPDFPage();
+//        CGContextRef pdfContext = UIGraphicsGetCurrentContext();
+//        
+//        
+//        // draws rect to the view and thus this is captured by UIGraphicsBeginPDFContextToData
+//        
+//        [aView.layer renderInContext:pdfContext];
+//        
+//        // remove PDF rendering context
+//        UIGraphicsEndPDFContext();
+//        
+//        // Retrieves the document directories from the iOS device
+//        NSArray* documentDirectories = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask,YES);
+//        
+//        NSString* documentDirectory = [documentDirectories objectAtIndex:0];
+//        NSString* documentDirectoryFilename = [documentDirectory stringByAppendingPathComponent:aFilename];
+//        
+//        // instructs the mutable data object to write its context to a file on disk
+//        [pdfData writeToFile:documentDirectoryFilename atomically:YES];
+//        NSLog(@"documentDirectoryFileName: %@",documentDirectoryFilename);
+        
+        
+        //swift 2.1 code
+        let pdfData = NSMutableData()
+        UIGraphicsBeginPDFContextToData(pdfData, aView.bounds, nil)
+        UIGraphicsBeginPDFPage()
+        
+        guard let pdfContext = UIGraphicsGetCurrentContext() else { return }
+        
+        aView.layer.render(in: pdfContext)
+        UIGraphicsEndPDFContext()
+        
+        if let documentDirectories = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+            let documentsFileName = documentDirectories + "/" + aFilename
+            debugPrint(documentsFileName)
+            pdfData.write(toFile: documentsFileName, atomically: true)
+        }
+    }
+    @IBAction func sendEmail(_ sender: UIButton) {
+        if let documentDirectories = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+            let documentsFileName : String = documentDirectories + "/" + "myFile.pdf"
+            debugPrint(documentsFileName)
+            
+            
+            //new code for sending email
+            if MFMailComposeViewController.canSendMail() {
+                let composer = MFMailComposeViewController()
+                composer.mailComposeDelegate = self
+                composer.setToRecipients([""])
+                composer.setMessageBody("Body", isHTML: false)
+                composer.setSubject("Subject")
+                let attachmentData = NSData(contentsOfFile: documentsFileName)
+//                let mimetype = self.mimeTypefrom(filePath: documentsFileName)
+                let mimetype = "application/pdf"
+//                composer.addAttachmentData(attachmentData! as Data, mimeType: mimetype, fileName: documentsFileName)
+                composer.addAttachmentData(attachmentData! as Data, mimeType: mimetype, fileName: "MyTaxResult.pdf")
+                self.present(composer, animated: true, completion: nil)
+            } else {
+                let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Check your mail configurations", delegate: self, cancelButtonTitle: "OK")
+                sendMailErrorAlert.show()
+            }
+            
+        }
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        print("finsih method")
+        controller.dismiss(animated: true)
+    }
+    
+//    func mimeTypefrom(filePath: String) -> String {
+//        
+//        let fileExtension = filePath.pathExtension;
+//        let UTI =  UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, fileExtension, nil)
+//        let mimeType = UTTypeCopyPreferredTagWithClass(UTI!.takeUnretainedValue(), kUTTagClassMIMEType);
+//        return (mimeType?.takeRetainedValue())! as String
+//    }
+    
+//    func exportHTMLContentToPDF(HTMLContent: String) {
+//        var pdfFilename = String()
+//        let printPageRenderer = CustomPrintPageRenderer()
+//        
+//        let printFormatter = UIMarkupTextPrintFormatter(markupText: HTMLContent)
+//        printPageRenderer.addPrintFormatter(printFormatter, startingAtPageAt: 0)
+//        
+//        let pdfData = drawPDFUsingPrintPageRenderer(printPageRenderer: printPageRenderer)
+//        
+//        pdfFilename = "\(AppDelegate.getAppDelegate().getDocDir())/Invoice\("myFile").pdf"
+//        pdfData?.write(toFile: pdfFilename, atomically: true)
+//        
+//        print(pdfFilename)
+//    }
+//    
+//    func drawPDFUsingPrintPageRenderer(printPageRenderer: UIPrintPageRenderer) -> NSData! {
+//        let data = NSMutableData()
+//        
+//        UIGraphicsBeginPDFContextToData(data, CGRect.zero, nil)
+//        
+//        UIGraphicsBeginPDFPage()
+//        
+//        printPageRenderer.drawPage(at: 0, in: UIGraphicsGetPDFContextBounds())
+//        
+//        UIGraphicsEndPDFContext()
+//        
+//        return data
+//    }
+    
     /*
     // MARK: - Navigation
 
